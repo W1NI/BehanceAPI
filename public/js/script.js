36f400018074491e1e9476127b9abb73464b9028
Unknown <charliematina@gmail.com>
2017-09-21 23:46:51 +1200

    Pulled Project
    Pulled a project and displayed the image as well as some other data. Added font awesome CDN.

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,4 @@
-var key = "PyMFCmfAybdTvwclB5XacPZxqbSOpYKV";
-// var url =  "http://behance.net/v2/users/"+behanceID+"?api_key="+key+"&callback=myCallbackFunction";
+var key = "pmWW5bXjxxOMdTZThQSloFafop81JSrA";
 
 function getBehanceData(behanceID){
 	$.ajax({
@@ -18,7 +17,49 @@ function getBehanceData(behanceID){
 						"</div>"+
 						"<div class='teamMemberName'>"+profileDisplayName+"</div>"+
 					"</div>"
-				)
+				);
+		},
+		error: function(){
+			console.log("Error with ajax request");
+		}
+	});
+};
+
+function getProjectImages(behanceID){
+	$.ajax({
+		url: "http://www.behance.net/v2/projects/56623139?client_id="+key,
+		contentType: "application/json",
+		dataType: "jsonp",
+		success: function(data){
+            console.log(data);
+            var projectName = data.project.name;
+            var projectUrl = data.project.url;
+			var projectLikes = data.project.stats.appreciations;
+			var projectComments = data.project.stats.comments;
+			var projectViews = data.project.stats.views;
+
+            $("#projectTitle").empty().append(projectName);
+            $("#projectImagesContainer").empty();
+
+			$("#projectStats").empty().append(
+				"<div class='stat views'><i class='fa fa-eye' aria-hidden='true'></i>"+projectViews+"</div>"+
+				"<div class='stat appreciations'><i class='fa fa-heart' aria-hidden='true'></i>"+projectLikes+"</div>"+
+				"<div class='stat comments'><i class='fa fa-comment' aria-hidden='true'></i>"+projectComments+"</div>"
+			);
+
+			$("#behanceLink").empty().append(
+				"<a href='"+projectUrl+"' target='_blank'>View on Behance</a><div class='linkHover'></div>"
+			);
+
+            for (var i = 0; i < data.project.modules.length; i++) {
+                var imageUrl = data.project.modules[i].sizes[1400];
+                $("#projectImagesContainer").append(
+                    "<div class='projectImage'>"+
+                        "<img src='"+imageUrl+"' alt=''>"+
+                    "</div>"
+                );
+
+            }
 		},
 		error: function(){
 			console.log("Error with ajax request");
@@ -26,10 +67,12 @@ function getBehanceData(behanceID){
 	});
 }
 
+
 $.get( "http://localhost:3000/behanceIDs", function(data) {
 	for (var i = 0; i < data.length; i++) {
 		var behanceID = data[i].behanceID;
 		getBehanceData(behanceID);
+		getProjectImages(behanceID);
 	}
 });
 
@@ -52,13 +95,18 @@ $(".waveContainer").fadeIn(1000);
 
 $("#landingPageContainer").fadeIn(600);
 
-$("#logo").click(function(){
-	scrollToTop();
-})
-
 scrollLoad("#serviceContent", 200, 1000);
 scrollLoad("#teamContent", 800, 1000);
 scrollLoad("#contactContent", 1200, 1000);
+
+$("#logo").click(function(){
+	scrollToTop();
+	closeOverlays();
+})
+
+$("#overlayTest").click(function(){
+	openProjectOverlay();
+})
 
 // Function which loads elements on scroll.
 function scrollLoad(element, value, timer){
@@ -73,3 +121,18 @@ function scrollLoad(element, value, timer){
 function scrollToTop(){
 	$('html, body').animate({scrollTop: 0}, 600);
 }
+
+// Closes all overlays currently open and sets mainContainer back to normal
+function closeOverlays(){
+	$("#overlayProfile").fadeOut(400);
+	$("#masterContainer").css("position","relative");
+};
+
+// Opens overlay for profile/projects
+function openProjectOverlay(){
+	$("#overlayProfile").css("display","flex");
+	$("#overlayProfile").hide();
+	$("#overlayProfile").fadeIn(700);
+	$("#masterContainer").css("position","fixed");
+
+};
